627d7135c03750abf71497faf0e90c73eaf86899
TD <IsaevSlava.2001@yandex.ru>
2023-11-25 09:00:34 +0000

    frontback

--- Hello.py
+++ Hello.py
@@ -17,6 +17,8 @@ from streamlit.logger import get_logger
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import pandas as pd
+from io import StringIO
 
 LOGGER = get_logger(__name__)
 
@@ -53,6 +55,24 @@ def run():
         page_title="Hello",
         page_icon="👋",
     )
+    
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+
     getInfo()
     st.image("growth.png")
 
